86578ba46ad6da46cd7138bd6ab4176095135c86
Olivier FLOD Systems <olivier@flod.aero>
2018-05-30 21:26:31 +0200

    begin of course to start recording

--- getTimeShift/getTimeShift.ino
+++ getTimeShift/getTimeShift.ino
@@ -1,9 +1,6 @@
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
-#include <TimeLib.h>
 
 // GUItool: begin automatically generated code
 AudioControlSGTL5000     sgtl5000_1;
@@ -20,21 +17,25 @@ const int myInput = AUDIO_INPUT_LINEIN;
 unsigned long ulngTime;
 
 void setup() {
-  AudioMemory(60);
+  AudioMemory(52);
   pinMode(13, OUTPUT);
 
   //Enable the audio shield
   sgtl5000_1.enable();
   sgtl5000_1.inputSelect(myInput);
+
+  fileL.begin(); // I forgot that shit
+  fileR.begin();
 }
 
 void loop(){
   int16_t bufferL[128];
   int16_t bufferR[128];
-
-  memcpy(bufferL, fileL.readBuffer(), 128);
+  int lIntBufferSize = 0;
+  lIntBufferSize = fileL.available();
+  memcpy(bufferL, fileL.readBuffer(), lIntBufferSize);
   fileL.freeBuffer();
-  memcpy(bufferR, fileR.readBuffer(), 128);
+  memcpy(bufferR, fileR.readBuffer(), fileR.available());
   fileR.freeBuffer();
  
   for (int i=0;i<128;i++){
