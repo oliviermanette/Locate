f113b755f3bad48804770bf511ae9d2644e48d16
Olivier FLOD Systems <olivier@flod.aero>
2018-06-13 21:47:25 +0200

    matlab avec boucle et affichage graphique

--- Test DATA/extractDataTest12juin2017.m
+++ Test DATA/extractDataTest12juin2017.m
@@ -2,9 +2,11 @@
 %position = 1;
 %trial = 1;
 GLOBAL_THRESHOLD  = 512;
-X = zeros(33,7);
-for position=1:6
-    for trial=1:2:9
+numberPos = 6;
+numberTrial = 7;
+X = zeros(numberPos*(numberTrial+1),7);
+for position=1:numberPos
+    for trial=1:2:((numberTrial*2)-1)
         tmpVr= eval(['peaks',num2str(position)]);
         [~, indiceT] = max(abs(tmpVr(:,trial))>512);
         [valeur, indice] = max(abs(tmpVr(:,trial)));
@@ -20,4 +22,13 @@ for position=1:6
         
         X(1+(position-1)*5+(trial-1),7) = position;
     end
+end
+strLesTitres=["Timing max";"Max Value";"Power"];
+for lintShift=1:3
+    figure(lintShift);
+    hold on; 
+    title(strLesTitres(lintShift));
+    plot(X(:,7),X(:,lintShift),'rx');
+    
+    plot(X(:,7),X(:,lintShift+3),'ko')
 end
