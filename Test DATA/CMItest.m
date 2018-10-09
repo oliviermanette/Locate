957d4cb71e670cf996a58b1237e27014716f73cf
Olivier FLOD Systems <olivier@flod.aero>
2018-10-09 12:20:54 +0200

    détection de 4 positions

--- Test DATA/CMItest.m
+++ Test DATA/CMItest.m
@@ -1,15 +1,19 @@
 %
 %plot(R,'r');hold on;plot(L,'b');
-position=4;
-graphcolor=['kx','rx','gx','bx'];
-tmpVr=eval(['R',num2str(position)]);
-tmpVl=eval(['L',num2str(position)]);
-[~,PositionsR]=max(abs(tmpVr(1:400,:)));
-[~,PositionsL]=max(abs(tmpVl(1:400,:)));
-rpos = PositionsL./PositionsR
-rmean = mean(abs(tmpVr))./mean(abs(tmpVl))
-figure(1);
-hold on
-plot(rpos,rmean,graphcolor(position));
-%rmax = max(abs(tmpVr))./max(abs(tmpVl))
-mean([rpos,rmean])
+graphcolor=['k*';'r*';'gx';'bx'];
+result=zeros(4,1);
+for position=1:4
+    position 
+    tmpVr=eval(['R',num2str(position)]);
+    tmpVl=eval(['L',num2str(position)]);
+    [~,PositionsR]=max(abs(tmpVr(1:400,:)));
+    [~,PositionsL]=max(abs(tmpVl(1:400,:)));
+    rpos = PositionsL./PositionsR
+    rmean = mean(abs(tmpVr))./mean(abs(tmpVl))
+    figure(1);
+    hold on
+    plot(rpos,rmean,graphcolor(position,:));
+    %rmax = max(abs(tmpVr))./max(abs(tmpVl))
+    result(position) = mean([rpos,rmean]);
+end
+result
